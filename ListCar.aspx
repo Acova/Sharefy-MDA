d94543649c3cabc258012962ca01ea2969557135
Néstor Ojeda González <nestorojedagonzalez@gmail.com>
2020-05-05 19:49:39 +0100

    List car

---
+++ ListCar.aspx
@@ -0,0 +1,127 @@
+﻿<%@ Page Title="Title" Language="C#" MasterPageFile="Site.Master" CodeBehind="ListCar.aspx.cs" Inherits="Sharefy_MDA.ListCar" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div id="success" class="alert alert-success" role="alert" runat="server">
+        Coche listado correctamente.
+    </div>
+
+    <div id="fail" class="alert alert-danger" role="alert" runat="server">
+        Ya existe un coche con esta matricula.
+    </div>
+
+    <div class="searchMenu">
+        <div class="row">
+            <div class="col">Matrícula</div>
+            <div class="col">
+                <input id="matriculaInput" type="text" class="form-control" placeholder="Matrícula*"
+                       aria-describedby="Button1" runat="server" style="min-width: 100px;"
+                       pattern="^[0-9]{1,4}(?!.*(LL|CH))[BCDFGHJKLMNPRSTVWXYZ]{3}"
+                       title="Debe ser una matricula española antigua o europea"/>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">Datos técnicos y condiciones</div>
+            <div class="col">
+                <input id="datosInput" type="text" class="form-control" placeholder="Datos*"
+                       aria-describedby="Button1" runat="server" style="min-width: 100px;"/>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">Fecha inicial de disponibilidad</div>
+            <div class="col">
+                <input id="InicioInput" type="date" class="form-control"
+                       aria-describedby="Button1" runat="server" style="min-width: 100px;"/>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">Fecha final de disponibilidad</div>
+            <div class="col">
+                <input id="FinInput" type="date" class="form-control"
+                       aria-describedby="Button1" runat="server" style="min-width: 100px;"/>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">Imagen</div>
+            <div class="col">
+                <asp:FileUpload runat="server" ID="flImage" aria-describedby="Button1" style="min-width: 100px;"/>
+                
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">Ciudad</div>
+            <div class="col">
+                <input id="ciudadInput" type="text" class="form-control" placeholder="Ciudad*"
+                       aria-describedby="Button1" runat="server" style="min-width: 100px;"/>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">Marca</div>
+            <div class="col">
+                <input id="marcaInput" type="text" class="form-control" placeholder="Marca*"
+                       aria-describedby="Button1" runat="server" style="min-width: 100px;"/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">Modelo</div>
+            <div class="col">
+                <input id="modeloInput" type="text" class="form-control" placeholder="Modelo*"
+                       aria-describedby="Button1" runat="server" style="min-width: 100px;"/>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">Potencia (en CV)</div>
+            <div class="col">
+                <input id="potenciaInput" type="text" class="form-control" placeholder="Potencia*"
+                       aria-describedby="Button1" runat="server" style="min-width: 100px;"/>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">Puertas</div>
+            <div class="col">
+                <input id="puertasInput" type="text" class="form-control" placeholder="Número de puertas*"
+                       aria-describedby="Button1" runat="server" style="min-width: 100px;"/>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">Tipo de coche</div>
+            <div class="col">
+
+                <select id="tipoInput" aria-describedby="Button1" style="min-width: 100px;" runat="server">
+                    <option value="Coupe">Coupe</option>
+                    <option value="Berlina">Berlina</option>
+                    <option value="Furgoneta">Furgoneta</option>
+                    <option value="Compacto">Compacto</option>
+                    <option value="Monovolumen">Monovolumen</option>
+                    <option value="Descapotable">Descapotable</option>
+                    <option value="Todoterreno">Todoterreno</option>
+                </select>
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">Precio</div>
+            <div class="col">
+                <input id="precioInput" type="text" class="form-control" placeholder="Precio*"
+                       aria-describedby="Button1" runat="server" style="min-width: 100px;"/>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-auto">
+                <asp:Button CssClass="btn btn-primary searchButton" type="button" ID="Button2" Text="Crear" runat="server" OnClick="Create"/>
+                <asp:Button CssClass="btn btn-primary searchButton" type="button" ID="Button1" Text="Cancelar" runat="server" OnClick="Cancel"/>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ ListCar.aspx.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Web;
+using System.Web.UI;
+
+namespace Sharefy_MDA
+{
+    public partial class ListCar : Page
+    {
+        protected System.Web.UI.HtmlControls.HtmlInputGenericControl InicioInput;
+        protected System.Web.UI.HtmlControls.HtmlInputGenericControl FinInput;
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            success.Visible = false;
+            fail.Visible = false;
+            InicioInput.Value = DateTime.Now.ToString("yyyy-MM-dd");
+            InicioInput.Attributes.Add("min", DateTime.Now.ToString("yyyy-MM-dd"));
+            FinInput.Attributes.Add("min", DateTime.Now.ToString("yyyy-MM-dd"));
+        }
+
+        protected void Create(object sender, EventArgs e)
+        {
+            if (Session["id"] != null)
+            {
+                if (CheckRequirements()) CreateCar(GetQuery());
+            }
+            else
+            {
+                fail.InnerText = "Tiene que estar registrado para poder listar coches";
+                fail.Visible = true;
+            }
+        }
+
+        protected void Cancel(object sender, EventArgs e)
+        {
+            Response.Redirect("/Profile?profile_id=" + Session["id"]);
+        }
+
+        private string GetQuery()
+        {
+            return GetBase() + GetValues();
+        }
+
+        private static string GetBase()
+        {
+            return
+                "insert into Coches (IDPropietario , Matricula, Datos, Inicio, Fin, Imagen, Ciudad, Marca, Modelo, Potencia, Puertas, Tipo, Precio) values (";
+        }
+
+        private static string GetValues()
+        {
+            return "@userId" + "," + "@plate" + "," + "@data" + "," + "@initDate" + "," + "@endDate" + "," + "@img" +
+                   "," + "@city" + "," + "@brand" + "," + "@model" + "," + "@power" + "," + "@doors" + "," + "@type" +
+                   "," + "@price" + ")";
+        }
+
+        private bool CheckRequirements()
+        {
+            var plate = matriculaInput.Value;
+            var data = datosInput.Value;
+            var initDate = InicioInput.Value;
+            var endDate = FinInput.Value;
+            var city = ciudadInput.Value;
+            var brand = marcaInput.Value;
+            var model = modeloInput.Value;
+            var power = potenciaInput.Value;
+            var doors = puertasInput.Value;
+            var price = precioInput.Value;
+
+            if (plate.Equals("") || data.Equals("") || initDate.Equals("") || city.Equals("") || brand.Equals("") ||
+                model.Equals("") || power.Equals("") || doors.Equals("") || price.Equals(""))
+            {
+                fail.InnerText = "Rellene los campos obligatorios";
+                fail.Visible = true;
+                return false;
+            }
+
+            if (!endDate.Equals(""))
+            {
+                var iDate = DateTime.ParseExact(initDate, "yyyy-MM-dd", null);
+                var eDate = DateTime.ParseExact(endDate, "yyyy-MM-dd", null);
+                if (DateTime.Compare(iDate, eDate) > 0)
+                {
+                    fail.InnerText = "La fecha de inicio no puede ser posterior a la de final";
+                    fail.Visible = true;
+                    return false;
+                }
+            }
+
+            if (!flImage.HasFile)
+            {
+                fail.InnerText = "Debe incluir una imagen del vehículo";
+                fail.Visible = true;
+                return false;
+            }
+
+            return true;
+        }
+
+        private void CreateCar(string sql)
+        {
+            var userId = Session["id"].ToString();
+            var plate = matriculaInput.Value;
+            var data = datosInput.Value;
+            var initDate = InicioInput.Value;
+            var endDate = FinInput.Value;
+            var city = ciudadInput.Value;
+            var brand = marcaInput.Value;
+            var model = modeloInput.Value;
+            var type = tipoInput.Value;
+            var power = potenciaInput.Value;
+            var doors = puertasInput.Value;
+            var price = precioInput.Value;
+            var imag = Image.FromStream(flImage.PostedFile.InputStream);
+
+            var relativeRoute = HttpContext.Current.Server.MapPath(@"\BDcoches.db");
+            var connstring = "data source=" + relativeRoute;
+            using (var db = new SQLiteConnection(connstring))
+            {
+                db.Open();
+
+                using (var cmd = new SQLiteCommand(sql, db))
+                {
+                    try
+                    {
+                        cmd.Parameters.Add("@userId", DbType.Int32).Value = userId;
+                        cmd.Parameters.Add("@plate", DbType.String).Value = plate;
+                        cmd.Parameters.Add("@data", DbType.String).Value = data;
+                        cmd.Parameters.Add("@initDate", DbType.String).Value = initDate;
+                        cmd.Parameters.Add("@endDate", DbType.String).Value = endDate;
+                        cmd.Parameters.Add("@img", DbType.Binary).Value =
+                            ConvertImageToByteArray(imag, ImageFormat.Png);
+                        cmd.Parameters.Add("@city", DbType.String).Value = city;
+                        cmd.Parameters.Add("@brand", DbType.String).Value = brand;
+                        cmd.Parameters.Add("@model", DbType.String).Value = model;
+                        cmd.Parameters.Add("@power", DbType.Int32).Value = power;
+                        cmd.Parameters.Add("@doors", DbType.Int32).Value = doors;
+                        cmd.Parameters.Add("@type", DbType.String).Value = type;
+                        cmd.Parameters.Add("@price", DbType.Int32).Value = price;
+                        cmd.ExecuteNonQuery();
+                        success.Visible = true;
+                    }
+                    catch
+                    {
+                        fail.InnerText = "Ya existe un vehiculo con esta matricula";
+                        fail.Visible = true;
+                    }
+
+                    Response.Redirect("/Profile?profile_id=" + Session["id"]);
+                }
+
+                db.Close();
+            }
+        }
+
+        private byte[] ConvertImageToByteArray(Image imageToConvert,
+            ImageFormat formatOfImage)
+        {
+            byte[] ret;
+            using (var ms = new MemoryStream())
+            {
+                imageToConvert.Save(ms, formatOfImage);
+                ret = ms.ToArray();
+            }
+
+            return ret;
+        }
+    }
+}
