1f87e39d7c8051cad2750c2ea65874bf3b290537
AlbertoDelRosario <alber996@hotmail.com>
2020-05-11 09:46:29 +0100

    Commit inicial 11-5

--- ListAds.aspx.designer.cs
+++ ListAds.aspx.designer.cs
@@ -15,6 +15,24 @@ namespace Sharefy_MDA
     {
 
         /// <summary>
+        /// Control carSearchInput.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText carSearchInput;
+
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
         /// Control cards.
         /// </summary>
         /// <remarks>
