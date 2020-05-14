40d55073d4363fb42617856914840b785d2e768b
RaymarML <raymarmanresa@gmail.com>
2020-05-14 17:33:31 +0100

    Complete Support Contact Functionality

--- Contact.aspx.cs
+++ Contact.aspx.cs
@@ -12,29 +12,57 @@ namespace Sharefy_MDA
     public partial class Contact : Page
     {
         String name = "";
-        String email = "";
         String text = "";
-        String sqlSentece = "insert into Mensages(Name,Email,Message,Attended) values(@name,@email,@text,@attended)";
+        String getSentece = "SELECT CUENTA FROM Usuarios WHERE CUENTA =";
+        String InsertSentece = "insert into Mensajes(Cuenta,Mensaje,Atendido) values(@account,@text,@attended)";
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Session["id"] == null) Response.Redirect("/Login.aspx");
         }
 
         public void send(object sender, EventArgs e)
         {
             collectData();
-            if (allInputsAreFilled()) {
+            if (allInputsAreFilled() && accountExist())
+            {
                 AddMessageToDataBase();
+                noticeUserSuccess();
+            }
+            else
+            {
+                noticeUserFail();
             }
             cleanData();
+            
+        }
+
+        public Boolean accountExist()
+        {
+            bool res = false;
+            var relativeRoute = HttpContext.Current.Server.MapPath(@"\BDcoches.db");
+            var connstring = "data source=" + relativeRoute;
+            using (var db = new SQLiteConnection(connstring))
+            {
+                db.Open();
+                String selectSentence = getSentece + "'" + name + "';";
+                using (var cmd = new SQLiteCommand(selectSentence, db))
+                {
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        if (reader.GetString(reader.FieldCount - 1).CompareTo(name) == 0)
+                        {
+                            res = true;
+                        }
+                    }
+                }
+                db.Close();
+            }
+            return res;
         }
 
         public Boolean allInputsAreFilled() {
             if (name == "") {
-                return false;
-            }
-            if (email == "")
-            {
                 return false;
             }
             if (text == "")
@@ -46,12 +74,29 @@ namespace Sharefy_MDA
 
         public void collectData()
         {
-            name = nameInput.Value;
-            email = mailInput.Value;
+            name = accountInput.Value;
             text = textArea.Value;
         }
 
-        public void cleanData() { }
+        public void cleanData() {
+            accountInput.Value = "";
+            textArea.Value = "";
+
+        }
+
+        public void noticeUserFail()
+        {
+            fail.InnerText = "La cuenta que ha indicado no existe";
+            fail.Visible = true;
+            success.Visible = false;
+        }
+
+        public void noticeUserSuccess()
+        {
+            success.InnerText = "Su mensaje se ha enviado";
+            success.Visible = true;
+            fail.Visible = false;
+        }
 
         private void AddMessageToDataBase()
         {
@@ -61,27 +106,21 @@ namespace Sharefy_MDA
             using (var db = new SQLiteConnection(connstring))
             {
                 db.Open();
-                using (var cmd = new SQLiteCommand(sqlSentece, db))
+
+                using (var cmd = new SQLiteCommand(InsertSentece, db))
                 {
                     try
                     {
-                        cmd.Parameters.Add("@name", DbType.String).Value = name;
-                        cmd.Parameters.Add("@email", DbType.String).Value = email;
+                        cmd.Parameters.Add("@account", DbType.String).Value = name;
                         cmd.Parameters.Add("@text", DbType.String).Value = text;
                         cmd.Parameters.Add("@attended", DbType.Int32).Value = 0;
-
- 
                         cmd.ExecuteNonQuery();
-
-
                     }
                     catch
-                    {
-                    }
-
-
-                    db.Close();
+                    { }
                 }
+                
+                db.Close();
             }
         }
     }
